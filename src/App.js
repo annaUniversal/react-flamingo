bd4787a55828363ed93271cc187386f46d01cdbc
annaUniversal <anna.universal@yahoo.com>
2023-11-05 19:14:40 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,5 @@
 import * as React from "react";
 
-let List = (props) => {
-  return (
-    <div>
-      <h2>{props.title}</h2>
-      <ul>
-        {props.list.map(function (item) {
-          return <Item item={item} />;
-        })}
-      </ul>
-    </div>
-  );
-};
-
-const Item = (props) => {
-  // or use const Item = ({item}) =>
-  //const item = props.item //if we don't want to use .
-  //const {item} = props - another way
-
-  return (
-    <li key={props.item.objectID}>
-      <span>
-        <a href={props.item.url}>{props.item.title}</a>
-      </span>
-      <span> {props.item.author} </span>
-      <span>{props.item.num_comments} </span>
-      <span>{props.item.points}</span>
-    </li>
-  );
-};
-
-function Search() {
-  const [searchTerm, setSearchTerm] = React.useState('');
-  
-  const handleChange = (event) => {
-
-    console.log(searchTerm);
-    setSearchTerm(event.target.value);
-    console.log(searchTerm);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input
-        id="search"
-        type="text"
-        onChange={handleChange}
-      />
-
-      <p>
-    Search for <strong>{searchTerm}</strong>
-      </p>
-    </div>
-  );
-}
-
 function App() {
   const stories = [
     {
@@ -95,10 +39,14 @@ function App() {
     },
   ];
 
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
     <div>
       <h1>My Stories</h1>
-      <Search />
+      <Search onSearch ={handleSearch}/>
       <hr />
       <List list={stories} title="React Ecosystem" />
       <List list = {javaScriptLibraries} title = "JS Libraries" />
@@ -106,4 +54,59 @@ function App() {
     </div>
   );
 }
+
+let List = (props) => {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <ul>
+        {props.list.map(function (item) {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const Item = (props) => {
+  // or use const Item = ({item}) =>
+  //const item = props.item //if we don't want to use .
+  //const {item} = props - another way
+
+  return (
+    <li key={props.item.objectID}>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span> {props.item.author} </span>
+      <span>{props.item.num_comments} </span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
+
+function Search(props) {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input
+        id="search"
+        type="text"
+        onChange={handleChange}
+      />
+
+      <p>
+    Search for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+}
+
 export default App;
