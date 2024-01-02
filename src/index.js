a58e62d6df60cee508be71a9db9c86311455d17b
Anna Gerhardt <annagerhardt@Annas-MBP.lan>
2024-01-02 18:34:43 -0500

    up to 22

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//i=i++;
 
 /*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( */
